36278bf5ea3c1572c92384942f7c803598d8bc01
nurbolott17 <nurbolot.nurbolot2002@mail.ru>
2025-02-02 16:07:32 +0300

    Файл с img

---
+++ hw3.js
@@ -0,0 +1,8 @@
+let pasworduser = 'Skypro01';
+let pasword = prompt('Введите пароль')
+
+if (pasword === pasworduser) {alert('Пароль введен верно')
+    
+} else {
+    alert('Пароль введен неправильно')
+} 
