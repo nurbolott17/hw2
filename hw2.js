660c503d9d0a55a66ebcc80d7a004c1a4ec56bd3
nurbolott17 <nurbolot.nurbolot2002@mail.ru>
2025-02-02 13:56:08 +0300

    first

---
+++ hw2.js
@@ -0,0 +1,56 @@
+// №1-Задание
+let a ;
+a=10;
+alert(a)
+a=20;
+alert(a);
+
+// #2-Задание
+let Data ='09.01.2007';
+alert(Data);
+
+// №3-Задание
+let name ='Брендан Эйх';
+alert(name);
+// №4-Задание
+let b;
+b = 10;
+let c =2;
+alert(b+c);
+alert(b-c);
+alert(b*c);
+alert(b%c);
+
+// №5-Задание
+let result = 2**5;
+alert(result);
+
+// №6-Задание 
+a = 9;
+b = 2;
+alert(a % b);
+// №7-Задание
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+// №8-Задание 
+let age = prompt('Сколько вам лет? ')
+alert(age)
+
+// №9-Задание
+let user = {
+    name:'maks',
+    age: 12,
+    isAdmin: true
+
+};
+
+name = prompt('Как тебя зовут?');
+alert('Привет,'+ name);
+
