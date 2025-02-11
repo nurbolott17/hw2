bbc7f18eab029a425f1ec080bca1ec5f4c728aa5
nurbolott17 <nurbolot.nurbolot2002@mail.ru>
2025-02-11 15:30:17 +0300

    первая игра готова

---
+++ hw5.js
@@ -0,0 +1,29 @@
+const answer = Math.floor((Math.random() * 100) + 1)
+console.log(answer)
+
+function guessTheNumber() {
+    userAnswer = parseInt(prompt('Угадай число то 1 до 100'))
+
+    while (userAnswer !== answer) {
+        
+        if (userAnswer === answer) {
+            
+            break;
+        } else  if (userAnswer > answer){
+            userAnswer = parseInt(prompt('Ты не угадал, число корторую я загодал меньше чем  ' + ' ' + userAnswer))
+            
+        }
+        else if (userAnswer < answer){
+            userAnswer = parseInt(prompt( 'Ты не угадал, число корторую я загодал ,больше чем '+ ' ' + userAnswer))       
+        }
+        else {
+            userAnswer = parseInt(prompt(userAnswer + '- Это не число , напиши число !!! '))
+        }
+        } 
+        alert('Молодец, ты угадал');   
+        console.log('Игра завершена')
+    }    
+
+console.log(answer)
+    
+   
